67b1623c61e644a8d731487021b8dd6f4fbd1c81
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-02-26 22:26:36 +0330

    add login and logout urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView, LibrarianRegisterView
+from .views import UserRegisterView, LibrarianRegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/user/', UserRegisterView.as_view()),
     path('register/librarian/', LibrarianRegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
